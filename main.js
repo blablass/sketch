bdc01b72886ad370e64b9670ee490bfb1f412761
blablass <82516957+blablass@users.noreply.github.com>
2022-06-15 15:54:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
+function preload(){
+    classifier = ml5.imageClassifier("DoddleNet")
 
+    
+}
 function setup(){
-    canvas = createCanvas(400,400)
+    canvas = createCanvas(280,280)
     canvas.center()
     background("brown")
+    canvas.mouseReleased(classifyCanvas)
+    synth = window.speechSynthesis
 }
 function clearsketch(){
     background("brown")
         
+}
+function draw(){
+    strokeWeight(10)
+    stroke("white")
+    if(mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY)
+    }
+}
+function classifyCanvas(){
+    classifier.classify(canvas,gotresult)
+}
+function gotresult(error,results){
+    if(error){
+        console.log(error)
+    }else{
+        console.log(results)
+        document.getElementById("label").innerHTML = "label:" + results[0].label
+        document.getElementById("confidence").innerHTML = "confidence:" + Math.floor(results[0].confidence*100)+ "%"
+        utter = new SpeechSynthesisUtterance(results[0].label)
+        synth.speak(utter)
+        
+    }
 }
